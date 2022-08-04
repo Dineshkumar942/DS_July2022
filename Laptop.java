32472668157688975c7abf3b74bec1c259d3de5e
Dineshkumar942 <71306052+Dineshkumar942@users.noreply.github.com>
2022-08-04 10:51:23 +0530

    Add files via upload

--- Laptop.java
+++ Laptop.java
@@ -1,43 +1,50 @@
-package com.practice;
-
-import java.util.Scanner;
 
 public class Laptop {
-
+	private String manufacturer;
+	private String RAM;
+	private int cost;
 	public static void main(String[] args) {
-		Lappy HP=new Lappy();
-		HP.setProps(45000, 8, "Pavilion");
-		HP.checkPrice();
-		HP.displayProps();
+		Laptop HP = new Laptop("HP", "6GB", 55000);
+		HP.InsertingRAM();
 	}
-
-}
-class Lappy{
-	float cost;
-	int RAM;
-	String model;
-	Scanner at=new Scanner(System.in);
-	void displayProps() {
-		System.out.println("Cost : "+cost+"/-");
-		System.out.println("RAM : "+ RAM+"GB");
-	    System.out.println("Model : "+model);
-	}
-	void setProps(float c,int r,String m) {
-		cost=c;
-		RAM=r;
-		model=m;
-	}
-	void checkPrice() {
-		System.out.println("Enter your budget: ");
-		float askPrice=at.nextFloat();
-		
-		if(askPrice < 50000) {
-			System.out.println("Laptop can be taken for less than 50k");
-		}
-		else {
-			System.out.println("Laptop is more than 50k");
-		}
+	public Laptop(String manufacturer, String rAM, int cost) {
+		super();
+		this.manufacturer = manufacturer;
+		RAM = rAM;
+		this.cost = cost;
 	}
 	
+	private MotherBoard ATX = new MotherBoard("Intel", "11th",true);
+	
+	public void InsertingRAM() {
+		System.out.println(ATX.canRAMBeExtendable(true));
+	}
+	
+	static class MotherBoard{
+		private String provider;
+		private String generation;
+		private boolean isRAMSlotAvailable;
+		
+		public MotherBoard(String provider, String generation, boolean isRAMSlotAvailable) {
+			super();
+			this.provider = provider;
+			this.generation = generation;
+			this.isRAMSlotAvailable = isRAMSlotAvailable;
+		}	
+		
+		String canRAMBeExtendable(boolean checker) {
+			this.isRAMSlotAvailable=checker;
+			if(this.isRAMSlotAvailable) {
+				return "YES Can be Extended";
+			}
+			else {
+				return "No space Available";
+			}
+		}
+		
+	}
+
+
+
 	
 }
